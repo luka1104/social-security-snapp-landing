cb96ad84f7daa5deaaa2c3cefe6a7d92ed642e94
luka1104 <skyward.luke11@gmail.com>
2023-04-16 04:26:20 +0900

    init chakra

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import * as React from "react"
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,4 @@
-import Head from "next/head"
-import Image from "next/image"
-import { Inter } from "next/font/google"
-import styles from "@/styles/Home.module.css"
 import SuperTokens from "@/components/SuperToken"
-
-const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
   return (
